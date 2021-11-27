85cf36147f5a6324f5003b88bca891f7843bcd71
Osvaldo Natercio Xavier da Silva <xavierdo@coventry.ac.uk>
2021-11-27 04:02:49 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,29 +1,72 @@
 from flask import Flask, abort
-import sqlite3 
+import sqlite3, os
 from flask import flash, session, render_template, request, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from hashlib import md5
+from flask_bcrypt import Bcrypt
 
+currentlocation = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(__name__)
 app.secret_key = "secret key"
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
+def homepage():
+    return render_template("homepage.html") #homepage
+
+@app.route('/', methods=['POST'])
 def login():
-    error = None
-    if request.method=='POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error=' Invalid Password, Please try again.'
-        else:
-           
-            return redirect(url_for('products'))
-    return render_template('login.html', error=error)
+    username = request.form['Username'] #request username and password
+    generate_password_hash = request.form['Password'] 
+    
+    sqlconnection = sqlite3.connect('store.db')
+    cursor = sqlconnection.cursor()
+    #check username and password if match with in the useers table then redirect to products
+    query1 = "SELECT Username, Password From users WHERE Username ='{un}' AND Password = '{ps}'".format(un=username, ps=generate_password_hash)
+    rows = cursor.execute(query1)
+    rows = rows.fetchall()
+    if len(rows)==1:
+        return redirect(url_for('products'))
+    else: 
+        return redirect(url_for('register')) #redirect to register page if user is not registered 
+    return render_template('homepage.html')
+    
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method =="POST":
+        dUn = request.form['Dusername'] #request username and password 
+        dPw = request.form['Dpassword']
+        sqlconnection = sqlite3.connect('store.db') #connect to the database 
+        cursor = sqlconnection.cursor()
+        query1 = "INSERT INTO users VALUES('{u}','{p}')".format(u=dUn, p=dPw) #store password into users table 
+        cursor.execute(query1)
+        sqlconnection.commit()
+        return redirect(url_for('homepage')) #redirect to the homepage for login users
+    return render_template('register.html')
+
+@app.route('/add_books', methods=['GET','POST'])
+def add_books(): #function use to store the books attributes into books table 
+    if request.method =="POST":
+        id = request.form['ID']
+        title = request.form['Title']
+        author = request.form['Author']
+        description = request.form['Description']
+        image = request.form['Image']
+        price = request.form['Price']
+        sqlconnection = sqlite3.connect('store.db')
+        cursor = sqlconnection.cursor()
+        query1 = "INSERT INTO books VALUES('{a}','{b}','{c}','{d}','{e}',{f})".format(a=id, b=title, c=author, d=description, e=image, f=price)
+        cursor.execute(query1)
+        sqlconnection.commit()
+        return redirect(url_for('products')) #redirect to the products page to display new items that has been added.
+    return render_template('add_books.html')
+
 @app.route('/products')
-def products():
+def products(): #showing all the books produtcs in the database to be displayed in product page.
     try:
-        con = sqlite3.connect('products.db')
+        con = sqlite3.connect('store.db')
         cur = con.cursor();
-        cur.execute("SELECT * FROM products")
+        cur.execute("SELECT * FROM books")
         rows = cur.fetchall()
         return render_template('products.html', products=rows)
     except Exception as e:
@@ -31,6 +74,7 @@ def products():
     finally:
         cur.close()
         con.close()
+
         
 sid = 'SJo_c3Nob3BwaW5nUGF5'
 pid = 'payment1'
@@ -40,15 +84,15 @@ secret = 'kvza9mEtztYV7i-WTtu-REwfilcA'
 def add_product_to_cart():
     cursor = None
     try:
-        _quantity = int(request.form['quantity'])
-        _code = request.form['code']
+        _quantity = int(request.form['quantity']) #based on ID or EAN number, quantity will be added depend on users clicked
+        _code = request.form['id']
         
-        if _quantity and _code and request.method == 'POST':
-            con = sqlite3.connect('products.db')
+        if _quantity and _code and request.method == 'POST': 
+            con = sqlite3.connect('store.db')
             cur = con.cursor();
-            cur.execute("SELECT * FROM products WHERE code=?;", [_code])
-            row = cur.fetchone()
-            itemArray = { row[2] : {'name' : row[1], 'code' : row[2], 'quantity' : _quantity, 'price' : row[4], 'image' : row[3], 'total_price': _quantity * row[4]}}
+            cur.execute("SELECT * FROM books WHERE id=?;", [_code])
+            row = cur.fetchone() #everytime user clicked the quantity will be added one into cart. 
+            itemArray = { row[0] : {'id' : row[0], 'title' : row[1], 'author' : row[2], 'description' : row[3],'quantity' : _quantity, 'image' : row[4], 'price' : row[5],  'total_price': _quantity * row[5]}}
             print('itemArray is', itemArray)
             
             all_total_price = 0
@@ -56,19 +100,19 @@ def add_product_to_cart():
             
             session.modified = True
             
-            if 'cart_item' in session:
+            if 'cart_item' in session: #if new item in cart_item 
                 print('in session')
-                if row[2] in session['cart_item']:
+                if row[0] in session['cart_item']:
                     for key, value in session['cart_item'].items():
-                        if row[2] == key:
-                            old_quantity = session['cart_item'][key]['quantity']
+                        if row[0] == key: #if ID equal to the key values of current item 
+                            old_quantity = session['cart_item'][key]['quantity']#old quantity will be added into quantity
                             total_quantity = old_quantity + _quantity
                             session['cart_item'][key]['quantity'] = total_quantity
-                            session['cart_item'][key]['total_price'] = total_quantity * row[4]
+                            session['cart_item'][key]['total_price'] = total_quantity * row[5] #total quantity * price of the products 
                 else:
                     session['cart_item'] = array_merge(session['cart_item'], itemArray)
                     
-                for key, value in session['cart_item'].items():
+                for key, value in session['cart_item'].items(): #when item presented in cart quantity and total_price will be added 
                     individual_quantity = int(session['cart_item'][key]['quantity'])
                     individual_price = float(session['cart_item'][key]['total_price'])
                     all_total_quantity = all_total_quantity + individual_quantity
@@ -76,7 +120,7 @@ def add_product_to_cart():
             else:
                 session['cart_item'] = itemArray
                 all_total_quantity = all_total_quantity + _quantity
-                all_total_price = all_total_price + _quantity * row[4]
+                all_total_price = all_total_price + _quantity * row[5] # if no new item added then it will show only current total price + quantity *price. 
                 
             session['all_total_quantity'] = all_total_quantity
             session['all_total_price'] = all_total_price
@@ -97,26 +141,25 @@ def add_product_to_cart():
     finally:
         cur.close()
         con.close()
-		
 
 
 @app.route('/empty')
-def empty_cart():
+def empty_cart(): #clear all the item in products using session and then redirect to products
 	try:
 		session.clear()
 		return redirect(url_for('.products'))
 	except Exception as e:
 		print(e)
 
-@app.route('/delete/<string:code>')
-def delete_product(code):
+@app.route('/delete/<string:id>')
+def delete_product(id): #delete item in the product based upon checking the id 
 	try:
 		all_total_price = 0
 		all_total_quantity = 0
 		session.modified = True
 		
 		for item in session['cart_item'].items():
-			if item[0] == code:				
+			if item[0] == id:				
 				session['cart_item'].pop(item[0], None)
 				if 'cart_item' in session:
 					for key, value in session['cart_item'].items():
--- make_db.py
+++ make_db.py
@@ -2,16 +2,20 @@ from flask import Flask
 from flask import render_template
 import sqlite3 
 
-con = sqlite3.connect('products.db')
+con = sqlite3.connect('store.db')
 
-con.execute('CREATE TABLE products(id INT unsigned, name VARCHAR(255), code VARCHAR(255), image TEXT, price DOUBLE)')
+con.execute('CREATE TABLE users(Username VARCHAR(255) , Password VARCHAR(255))')
+con.close() 
+
+'''con.execute('CREATE TABLE books(id VARCHAR(255) PRIMARY KEY, title VARCHAR(255), author VARCHAR(255), description VARCHAR(255),image TEXT, price DOUBLE)')
 con.close()  
 
 
-con = sqlite3.connect('products.db')
-con.execute('INSERT INTO products(id, name, code, image, price) VALUES (1, "American Tourist", "AMTR01", "product-images/bag.jpg", 12000.00),(2, "EXP Portable Hard Drive", "USB02", "product-images/external-hard-drive.jpg", 5000.00),(3, "Shoes", "SH03", "product-images/shoes.jpg", 1000.00),(4, "XP 1155 Intel Core Laptop", "LPN4", "product-images/laptop.jpg", 80000.00),(5, "FinePix Pro2 3D Camera", "3DCAM01", "product-images/camera.jpg", 150000.00),(6, "Simple Mobile", "MB06", "product-images/mobile.jpg", 3000.00),(7, "Luxury Ultra thin Wrist Watch", "WristWear03", "product-images/watch.jpg", 3000.00),(8, "Headphones", "HD08", "product-images/headphone.jpg", 400.00);')
+con = sqlite3.connect('store.db')
+con.execute('INSERT INTO books(id, title, author, description, image, price) VALUES (9780241466940, "The Fortune Men: Shortlisted for the Booker Prize 2021", "Nadifa Mohamed", "Chilling and utterly compelling , The Fortune Men shines an essential light on a much-neglected period of our national life Sathnam Sanghera","product-images/The Fortune Men.jpg", 12.50),(9781910688922, "An Island", "Karen Jennings ","An Island concerns itself with lives lived on the margins, through the story of a man who has exiled himself from the known world only to find himself called to the service of others", "product-images/An Island.jpg", 10.00),(9781787333345, "Silent Earth: Averting the Insect Apocalypse", "Dave Goul","We have to learn to live as part of nature, not apart from it. And the first step is to start looking after the insects, the little creatures that make our shared world go round. Insects are essential for life as we know it", "product-images/Silent Earth.jpg", 15.25),(9781847924018, "Four Thousand Weeks: Embrace your limits. Change your life.", "Oliver Burkeman ","We are obsessed with our lengthening to-do lists, our overfilled inboxes, the struggle against distraction, and the sense that our attention spans are shrivelling. Still, we rarely make the connection between our daily struggles with time and the ultimate time management problem: the question of how best to use our ridiculously brief time on the planet, which amounts on average to about four thousand weeks", "product-images/Four Thousand Weeks.jpg", 15.79),(9780241381861, "Connections: A Story of Human Feeling", "Karl Deisseroth","In this riveting journey through the hidden realms of the human mind, a world-renowned psychiatrist and neuroscientist explores the origins of human emotion, and examines what mental illnesses reveal about all of us ", "product-images/Connections.jpg", 18.60),(9780571368983, "Why Solange Matters", "Stephanie Phillips ","The dramatic story of Solange: a musician and artist whose unconventional journey to international success was far more important than her family name.", "product-images/Why Solange Matters.jpg", 9.29),(9780241243213, "Antwerp: The Glory Years", "Michael Pye","A rich history of Antwerp from the author of the acclaimed bestseller The Edge of the World Even before Amsterdam there was a dazzling North Sea port at the hub of the city of Antwerp.", "product-images/Antwerp.jpg", 23.25),(9781846148248, "The World According to Colour: A Cultural History", "James Fox ","A beguiling cultural history of colour by the BAFTA nominated broadcaster and art historian James Fox", "product-images/The World According to Colour.jpg", 23.25),(9781529388626, "Earthshot: How to Save Our Planet", "Colin Butfield","The Earthshot concept is simple: Urgency + Optimism = Action . We have ten years to turn the tide on the environmental crisis, but we need the worlds best solutions and one shared goal - to save our planet.", "product-images/Earthshot.jpg", 18.60),(9781787417663, "Art of Protest: What a Revolution Looks Like", "De Nichols ","From the psychedelic typography used in Make Love Not War posters of the 60s, to the solitary raised fist, take a long, hard look at some of the most memorable and striking", "product-images/Art of Protest.jpg", 15.79);')
+            
 con.commit()
-
+'''
 con.close()  
 
 
